19761064b1df0c944ac9cab3e6d53a4b4663e79c
sar2160 <srimmele@gmail.com>
2016-05-01 12:51:18 -0400

    turn debug off

--- local_settings.py
+++ local_settings.py
@@ -9,7 +9,7 @@ API Keys and other parameters
 ## Twitter details 
 CONSUMER_KEY = 'p6RG6xLzyxzr6T50xMEvWAV66'
 CONSUMER_SECRET ='mFnPcOuALBPzV7obdWGNr4oYClkjsi8AV7FaNP0ID28hBvrJbZ'
-ACCESS_KEY = '717142719223697408-eVDSpMRGUzyareFx1HZv0K6EOKiB7vk' #keep the quotes, replace this with your access token
+ACCESS_KEY = '717142719223697408-eVDSpMRGUzyareFx1HZv0K6EOKiB7vk' 
 ACCESS_SECRET = 'Bk7zKGjJ7y314KcgVP55HbhwQOs1ns4JKbOBn5en55XmS'
 
 
@@ -18,7 +18,7 @@ max_wait = 1800 # 30 minutes
 
 
 ## debug mode does not tweet, prints to console
-DEBUG = True
+DEBUG = False
 
 # delete some records to force a tweet/print
 TEST_DATA = False
